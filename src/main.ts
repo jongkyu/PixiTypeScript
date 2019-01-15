8efd2d34a6e51ce863ddad9dc58e623d30b2ad7d
userB <brane7@hncgames.com>
2019-01-15 13:35:52 +0900

    Pixi TypeScript Webpack & watch

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import * as PIXI from "pixi.js";
+
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
+
+console.log("Start");
