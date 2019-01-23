57926409d1439c46d14fc9528ab9d0641cfebfb6
brane7 <brane7@hncgames.com>
2019-01-23 18:33:15 +0900

    현재 해상도 관련 테스트

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,7 @@
 
 import * as PIXI from "pixi.js";
+import 'pixi-spine';
+
 import { Sub, SubCar, SubHot } from './sub';
 import { CoroutineManager, WaitForSeconds } from './CoroutineManager';
 import { Util } from "./Util";
@@ -12,6 +14,7 @@ export class Main {
 
     private platformType: string = "Car";
 
+    private dragon: PIXI.spine.Spine;
     constructor() {
         console.log("Main Init");
         this.init();
@@ -19,8 +22,8 @@ export class Main {
 
     init() {
         this.app = new PIXI.Application({
-            width: 1000,
-            height: 1000,
+            width: 846,
+            height: 412,
             antialias: true,
             transparent: false,
             resolution: 1
@@ -28,23 +31,70 @@ export class Main {
         );
 
         this.initCoroutine();
-
+        this.initTextField();
 
         document.body.appendChild(this.app.view);
 
         console.log("init");
         console.log(this.app);
 
+
+
+        // 일반 이미지 로드 
+        // PIXI.loader
+        //     .add("images/cat.png")
+        //     .load(() => {
+        //         this.setup();
+        //     });
+
+        //spine로드
         PIXI.loader
-            .add("images/cat.png")
-            .load(() => {
-                this.setup();
+            .add('power_shoot', 'images/spine/power_shoot.json')
+            .load((loader: any, res: any) => {
+                this.onAssetsLoaded(loader, res);
             });
 
         this.app.ticker.add((deltaTime: number) => {
             this.update(deltaTime);
         })
 
+    }
+
+    public onAssetsLoaded(loader: PIXI.loaders.Loader, res: any) {
+
+
+        console.log(res);
+        this.dragon = new PIXI.spine.Spine(res.power_shoot.spineData);
+        this.dragon.skeleton.setToSetupPose();
+        this.dragon.update(0);
+        this.dragon.autoUpdate = false;
+
+        // create a container for the spine animation and add the animation to it
+        var dragonCage = new PIXI.Container();
+        dragonCage.addChild(this.dragon);
+
+        // measure the spine animation and position it inside its container to align it to the origin
+        var localRect = this.dragon.getLocalBounds();
+        this.dragon.position.set(-localRect.x, -localRect.y);
+
+        // now we can scale, position and rotate the container as any other display object
+        var scale = Math.min(
+            (this.app.screen.width * 0.7) / dragonCage.width,
+            (this.app.screen.height * 0.7) / dragonCage.height
+        );
+        dragonCage.scale.set(scale, scale);
+        dragonCage.position.set(
+            (this.app.screen.width - dragonCage.width) * 0.5,
+            (this.app.screen.height - dragonCage.height) * 0.5
+        );
+
+        // add the container to the stage
+        this.app.stage.addChild(dragonCage);
+
+        // once position and scaled, set the animation to play
+        this.dragon.state.setAnimation(0, 'play_02', true);
+
+        //this.app.start();
     }
 
     private initCoroutine() {
@@ -55,6 +105,8 @@ export class Main {
 
     private update(deltaTime: number) {
         if (this.containerCar) this.containerCar.rotation += 0.01;
+
+        if (this.dragon) this.dragon.update(0.016);
     }
 
 
@@ -129,6 +181,34 @@ export class Main {
             console.log(v + " " + ms + "ms");
             return v;
         });
+    }
+
+
+    initTextField() {
+        var style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#ffffff', '#00ff99'], // gradient
+            stroke: '#4a1850',
+            strokeThickness: 5,
+            dropShadow: true,
+            dropShadowColor: '#000000',
+            dropShadowBlur: 4,
+            dropShadowAngle: Math.PI / 6,
+            dropShadowDistance: 6,
+            wordWrap: true,
+            wordWrapWidth: 440
+        });
+
+        let windowOuterWidth = window.innerWidth;
+        let windowOuterHeight = window.innerHeight;
+        var richText = new PIXI.Text(windowOuterWidth + " = " + windowOuterHeight, style);
+        richText.x = 0;
+        richText.y = 0;
+
+        this.app.stage.addChild(richText);
     }
 }
 
